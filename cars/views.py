df3034959dd328033b69f9c259c875bb74d84b81
yuri noriki <yurinoriki@hotmail.com>
2025-03-13 17:28:04 -0300

    Buscando carros no banco de dados com django ORM

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from cars.models import Car
+
 
 def cars_views(request):
+    cars = Car.objects.all()
+
+    context = {
+    'cars': cars,
+}
+
     return render(
         request,
         'cars.html',
+        context,
     )
