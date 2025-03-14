d5b5ffa929d666678d00f59d6a1849713549f578
yuri noriki <yurinoriki@hotmail.com>
2025-03-14 14:51:52 -0300

    Filtrando carros com os parametros do usuario

--- cars/views.py
+++ cars/views.py
@@ -3,10 +3,14 @@ from cars.models import Car
 
 
 def cars_views(request):
-
-    print(request.GET.get('search'))
-
-    cars = Car.objects.filter(model__icontains = 'a')
+    # Primeiro buscou todos os carros
+    cars = Car.objects.all()
+    # Verifica se o usuario mandou alguma busca
+    search = request.GET.get('search')
+    # Se mandou,
+    if search:
+        #cars. filter vai filtrar os carros que ja mandou buscar(cars = Car.objects.all)
+        cars = cars.filter(model__contains = search)
 
     context = {
     'cars': cars,
