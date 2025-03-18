027e908d83e4eff524e912f1d09dd0e8b7647326
yuri noriki <yurinoriki@hotmail.com>
2025-03-18 17:14:43 -0300

    organizando as pastas do projeto carros e criando nosso forms(formulario)

--- app/urls.py
+++ app/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import cars_views
+from cars.views import cars_views,new_cars_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', cars_views, name = 'cars_list'),
+    path('new_cars/', new_cars_views, name = 'new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,23 +1,38 @@
 from django.shortcuts import render
 from cars.models import Car
+from cars.forms import CarForm
 
 
 def cars_views(request):
     # Primeiro buscou todos os carros
     cars = Car.objects.all().order_by('model')
     # Verifica se o usuario mandou alguma busca
-    search = request.GET.get('search')
+    search = request.GET.get('search') 
     # Se mandou,
     if search:
         #cars. filter vai filtrar os carros que ja mandou buscar(cars = Car.objects.all)
         cars = cars.filter(model__contains = search)
-
+        
+        
     context = {
     'cars': cars,
-}
+    }
 
     return render(
         request,
         'cars.html',
+        context,
+    )
+
+def new_cars_views(request):
+    new_cars_form = CarForm()
+
+    context = {
+        'new_cars_form': new_cars_form,
+    }
+
+    return render(
+        request,
+        'new_cars.html',
         context,
     )
