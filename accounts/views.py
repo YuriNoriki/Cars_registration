25657e25ff0f17814fb78273dba5bf13f3ee07e5
yuri noriki <yurinoriki@hotmail.com>
2025-03-23 23:41:46 -0300

    Criando tela de login

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import authenticate,login,logout
 
 def register_views(request):
     if request.method == "POST":
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
-            redirect('cars_list')
+            redirect('login')
     else:
         user_form = UserCreationForm()
 
@@ -19,3 +20,31 @@ def register_views(request):
         'register.html',
         context,
     )
+
+def login_views(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('cars_list')
+        else:
+            login_form = AuthenticationForm()
+    else:        
+        login_form = AuthenticationForm()
+
+    context = {
+        'login_form': login_form
+    }
+
+    return render(
+        request,
+        'login.html',
+        context,
+    )
+
+def logout_views(request):
+    logout(request)
+    return redirect('login')
