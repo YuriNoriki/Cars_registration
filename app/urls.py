3df9a7c0b81a8c0745c6d5a19b42608cb9d68684
yuri noriki <yurinoriki@hotmail.com>
2025-04-08 17:36:14 -0300

    Utilizando update

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarListView,NewCreateCar, CarDetailView
+from cars.views import CarListView,NewCreateCar, CarDetailView, carUpdateView
 from accounts.views import register_views, login_views, logout_views
 
 urlpatterns = [
@@ -29,4 +29,5 @@ urlpatterns = [
     path('cars/', CarListView.as_view(), name = 'cars_list'),
     path('new_cars/', NewCreateCar.as_view(), name = 'new_car'),
     path('car/<int:pk>/', CarDetailView.as_view(), name = 'car_detail'),
+    path('car/<int:pk>/update', carUpdateView.as_view(), name = 'car_update'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
